fd4dd4da56c19d46d5241889eac1224c6dea1145
Anthony Micco <anthonymicco2003@gmail.com>
2023-02-13 20:47:32 -0500

    changes

--- firstwebsite/src/App.js
+++ firstwebsite/src/App.js
@@ -2,16 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './App.css'
 import CurrencyRow from './CurrencyRow';
 
-const BASE_URL = 'https//api.exchangeratesapi.io/v1/latest'
-
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([])
   console.log(currencyOptions)
-
-  useEffect(() => {
-
-
-  }, [])
 
   return (
     <>
