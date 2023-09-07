f3a3378845d2d87a6e26a65900d2062292941c36
Anthony Micco <anthonymicco2003@gmail.com>
2023-09-07 13:38:57 -0400

    more python practice

--- python_practice/firstpyprogram.py
+++ python_practice/firstpyprogram.py
@@ -1,1 +1,15 @@
-print("Hello World")
+a = "The big brown dog jumped over the blue moon"
+b = 1000
+c = 2000
+true = "{} is bigger than {}"
+false = "{} is not bigger than {}"
+
+if "black" in a:
+    print("The word black is in " + a)
+else:
+    print("The word black is not in a")
+    
+if c > b:
+    print(true.format(c,b))
+else: 
+    print(false.format(c,b))
