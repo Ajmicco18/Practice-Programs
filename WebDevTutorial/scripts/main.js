ba0d0bcf87349908cc70ae7f82a2bb53e7a21dcd
Anthony Micco <anthonymicco2003@gmail.com>
2023-10-18 19:36:41 -0400

    working on JS

--- WebDevTutorial/scripts/main.js
+++ WebDevTutorial/scripts/main.js
@@ -1,2 +1,18 @@
+//DOM = Document Object Model
+/*
+    -document.getElementsByTagName()
+    -doucment.getElementByID()
+    -document.getElementsByClassName()
+    -document.querySelector(Any HTML element)
+    -document.querySelectorAll
+    -html.innerHTML = "" changes content inside HTML  
+*/
+
+var pTags = document.getElementsByTagName('p')
 
 var myList = ['apples', 'oranges', 'bananas'];
+
+
+for (var i = 0; i < myList.length; i++) {
+    console.log("You have " + myList[i] + " in your basket.");
+}
