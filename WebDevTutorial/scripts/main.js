ef4c0f4f0fe62cdfa03ae3415974221d1ab9ae1c
Anthony Micco <anthonymicco2003@gmail.com>
2023-10-19 13:39:04 -0400

    continued working with JS

--- WebDevTutorial/scripts/main.js
+++ WebDevTutorial/scripts/main.js
@@ -8,11 +8,45 @@
     -html.innerHTML = "" changes content inside HTML  
 */
 
-var pTags = document.getElementsByTagName('p')
 
-var myList = ['apples', 'oranges', 'bananas'];
+var numOne = document.getElementById("num-one");
+var numTwo = document.getElementById("num-two");
+var addDifference = document.getElementById("difference");
 
+var addOne = document.getElementById("add-one");
+var addTwo = document.getElementById("add-two");
+var addSum = document.getElementById("sum")
 
-for (var i = 0; i < myList.length; i++) {
-    console.log("You have " + myList[i] + " in your basket.");
+var mOne = document.getElementById("mult-one");
+var mTwo = document.getElementById("mult-two");
+var addProduct = document.getElementById("product");
+
+var bTags = document.getElementById('change');
+
+numOne.addEventListener("input", subtract);
+numTwo.addEventListener("input", subtract);
+
+addOne.addEventListener("input", add);
+addTwo.addEventListener("input", add);
+
+mOne.addEventListener("input", multiply);
+mTwo.addEventListener("input", multiply);
+
+function subtract() {
+
+    var one = parseFloat(numOne.value) || 0;
+    var two = parseFloat(numTwo.value) || 0;
+    addDifference.innerHTML = "The difference is: " + (one - two);
+}
+
+function add() {
+    var oneAdd = parseFloat(addOne.value) || 0;
+    var twoAdd = parseFloat(addTwo.value) || 0;
+    addSum.innerHTML = "The sum is: " + (oneAdd + twoAdd);
+}
+
+function multiply() {
+    var oneMult = parseFloat(mOne.value) || 0;
+    var twoMult = parseFloat(mTwo.value) || 0;
+    addProduct.innerHTML = "The product is: " + (oneMult * twoMult);
 }
