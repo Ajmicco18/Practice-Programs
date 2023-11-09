4868847a250785db39d9be280fc2c2ef591a8701
Anthony Micco <anthonymicco2003@gmail.com>
2023-11-09 11:13:57 -0500

    file changes

--- reactTutorial/todo-list/src/App.js
+++ reactTutorial/todo-list/src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import InComponent from './components/InComponent';
-import StyleSheet from './components/StyleSheet';
-import Todo from './components/Todo';
 
 function App() {
   return (
     <div className='App'>
-      <StyleSheet />
     </div>
   );
 }
